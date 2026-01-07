61500422bf76db203ee62c4e3d7a9d48b9db28ed
Melanythevision07 <melanyalejandra0721@gmail.com>
2026-01-07 14:49:19 -0500

    descuento segu la edad

--- descuento.py
+++ descuento.py
@@ -1,1 +1,17 @@
-n la eda
+#verificacion de edad para descuento
+edad = 67
+compra = 80000
+porcentaje = 0.20
+descuento = compra * porcentaje
+
+#verificar
+if edad >= 65 and compra > 50000:
+    print("Califica para descuento.")
+    total = compra - descuento
+else:
+    print("No califica para descuento.")
+    total = compra
+    
+#formato
+total_formato = f"${:,.0f}" .replace(",",".")
+print ("precio final:", total_formato)
