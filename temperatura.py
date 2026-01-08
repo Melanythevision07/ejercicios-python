dbcafd6abe714b1735bcda07c803cd22d62fcf89
Melanythevision07 <melanyalejandra0721@gmail.com>
2026-01-08 10:56:22 -0500

    convertir la temperarura de medellin

---
+++ temperatura.py
@@ -0,0 +1,11 @@
+#conversion de temperatura
+temperatura = 28
+f = (temperatura * 9/5) + 32
+print("La temperatura es:", f)
+
+#verificar temperatura
+if f >= 86:
+    print("Hace mucho calor")
+else:
+  
+    print("Hace calor")
